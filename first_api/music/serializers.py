88982f8f5da2edbe5e7a4efc961063033f5e05e4
Fang Yi Liu <fangyiliu@gmail.com>
2024-09-29 02:13:30 -0700

    Add artist serializer and view

---
+++ first_api/music/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from music.models import Artist
+
+class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['name']
